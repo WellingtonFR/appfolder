e8d191e466f1456197d260964cd5217c5e64be10
WellingtonFR <wellingtondfr@gmail.com>
2024-08-06 22:44:45 -0300

    inclusão de api routes para fins de estudo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "app folder",
   description: "Criado para estudo da pasta app e do server side rendering e client side rendering",
+  keywords: ["HTML", "CSS", "JAVASCRIPT", "PROGRAMAÇÃO"],
+  openGraph: {
+    images: [""],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
 };
 
 export default function RootLayout({
